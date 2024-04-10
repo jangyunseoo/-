541a2133583e205d04d23fb04af57e0516b0857b
jangyunseoo <163449148+jangyunseoo@users.noreply.github.com>
2024-04-10 18:14:11 +0900

    hw4.py

--- hw4.py
+++ hw4.py
@@ -7,6 +7,5 @@ msg1='Hello '+name+','
 msg2='Welcome to Seoul.'
 nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
 draw_line_string()
-print(msg1)
-print(msg2)
+print(msg1+'\n'+msg2)
 draw_line_string()
