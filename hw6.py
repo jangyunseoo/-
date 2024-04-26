958463134d8c3083278c17a318d9fe034bcc2171
jangyunseoo <163449148+jangyunseoo@users.noreply.github.com>
2024-04-26 11:39:46 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,6 @@
+for i in range(1, 10) :
+    for j in range(2, 6) :
+        print(j, 'x', i, '=', j * i, end = '\t')
+    for j in range(6, 10) :
+        print(j, 'x', i, '=', j * i, end = '\t')
+    print()
