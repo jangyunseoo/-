f7f0745e5c9044a47ee5914339c0e511790413cd
jangyunseoo <163449148+jangyunseoo@users.noreply.github.com>
2024-04-26 11:08:53 +0900

    hw4.py

--- hw4.py
+++ hw4.py
@@ -6,8 +6,8 @@ def draw_line_string(name) :
     msg2 = 'Welcome to Seoul.'
     nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
     rep_char('-', nstr + 2)
-    print(f' {msg1} ')
-    print(f' {msg2} ')
+    print(f'{msg1}')
+    print(f'{msg2}')
     rep_char('-', nstr + 2)
     
 n = input('Input his/her name: ')
