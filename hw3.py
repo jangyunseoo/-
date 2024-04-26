d783c546c28c5a5554d9a8c0e6903b23d88e3a3a
jangyunseoo <163449148+jangyunseoo@users.noreply.github.com>
2024-04-26 10:35:24 +0900

    hw3.py

--- hw3.py
+++ hw3.py
@@ -1,11 +1,11 @@
-def get_fixed_price(before):
-    percent=int(input(prompt))
-    discount=int(input(prompt))
-    before=discount/((100-percent)/100)
-percent=int(input('할인율은? '))
-discount=int(input('A 상품의 할인된 가격은? '))
-before=discount/((100-percent)/100)
-discount=int(input('B 상품의 할인된 가격은? '))
-print('A 상품의 정가는', before, '원')
-before=discount/((100-percent)/100)
-print('B 상품의 정가는', before, '원')
+def get_fixed_price(price, rate) :
+    fixed_price = price * 100 // (100 - rate)
+    return fixed_price
+
+sale_rate = int(input('할인율은? '))
+sale_price1 = int(input('A 상품의 할인된 가격은? '))
+sale_price2 = int(input('B 상품의 할인된 가격은? '))
+fixed_price1 = get_fixed_price(sale_price1, sale_rate)
+print('A 상품의 정가는', fixed_price1, '원')
+fixed_price2 = get_fixed_price(sale_price2, sale_rate)
+print('B 상품의 정가는', fixed_price2, '원')
