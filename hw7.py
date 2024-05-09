a1ce0920cc8ef6088acff805a7929a54e3607f85
jangyunseoo <163449148+jangyunseoo@users.noreply.github.com>
2024-05-09 16:18:32 +0900

    Add files via upload

---
+++ hw7.py
@@ -0,0 +1,18 @@
+shopping_bag = []
+print('[구입]')
+while True :
+    item = input('상품명? ')
+    if item == '' :
+        break
+    amount = input('수량은? ')
+    a = {item : amount}
+    shopping_bag.append(a)
+    print(f'장바구니에 {item} {amount}개가 담겼습니다.\n')
+
+print(f'\n>>> 장바구니 보기: {shopping_bag}')
+
+print('\n[검색]')
+check = input('장바구니에서 확인하고자 하는 상품은? ')
+if check in shopping_bag :
+    idx = shopping_bag.index(check)
+    print(f'{check}은(는) {idx + 1}개 담겨 있습니다.')
