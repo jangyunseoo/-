729394923b15ef01aa7f7c614b7775f86a40cca2
jangyunseoo <163449148+jangyunseoo@users.noreply.github.com>
2024-05-17 20:52:35 +0900

    hw7.py

--- hw7.py
+++ hw7.py
@@ -1,12 +1,12 @@
-shopping_bag = []
+shopping_bag = {}
 print('[구입]')
+
 while True :
     item = input('상품명? ')
     if item == '' :
         break
     amount = input('수량은? ')
-    a = {item : amount}
-    shopping_bag.append(a)
+    shopping_bag[item] = amount
     print(f'장바구니에 {item} {amount}개가 담겼습니다.\n')
 
 print(f'\n>>> 장바구니 보기: {shopping_bag}')
@@ -14,5 +14,4 @@ print(f'\n>>> 장바구니 보기: {shopping_bag}')
 print('\n[검색]')
 check = input('장바구니에서 확인하고자 하는 상품은? ')
 if check in shopping_bag :
-    idx = shopping_bag.index(check)
-    print(f'{check}은(는) {idx + 1}개 담겨 있습니다.')
+    print(f'{check}은(는) {amount}개 담겨 있습니다.')
